286e7f0b4979d624dd71a3f91a958869ac354a7e
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2024-10-04 13:16:26 +0200

    fix(discid): Fix serialization for `Disc` and `Discid`

--- src/entity/discid.rs
+++ src/entity/discid.rs
@@ -4,7 +4,11 @@ use serde::{Deserialize, Serialize};
 
 /// Disc ID is the code number which MusicBrainz uses to link a physical CD to a release listing.
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
-#[serde(rename_all(deserialize = "kebab-case"))]
+#[cfg_attr(
+    feature = "legacy_serialize",
+    serde(rename_all(deserialize = "kebab-case"))
+)]
+#[cfg_attr(not(feature = "legacy_serialize"), serde(rename_all = "kebab-case"))]
 pub struct Discid {
     /// See [MusicBrainz Identifier](https://musicbrainz.org/doc/MusicBrainz_Identifier).
     pub id: String,
@@ -15,7 +19,11 @@ pub struct Discid {
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
-#[serde(rename_all(deserialize = "kebab-case"))]
+#[cfg_attr(
+    feature = "legacy_serialize",
+    serde(rename_all(deserialize = "kebab-case"))
+)]
+#[cfg_attr(not(feature = "legacy_serialize"), serde(rename_all = "kebab-case"))]
 pub struct Disc {
     /// See [MusicBrainz Identifier](https://musicbrainz.org/doc/MusicBrainz_Identifier).
     pub id: String,
